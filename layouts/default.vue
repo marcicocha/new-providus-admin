5cd2c25cc18f2ef4e7b93d57d4c4d1c715d2f815
unknown <marcia.cole@longbridgetech.com>
2021-07-08 02:42:16 +0100

    fix: some update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout id="components-layout-demo-responsive">
-    <AppAdminSideBar />
+    <AppAdminSideBar @showChangePasswordModal="showChangePasswordModal" />
     <a-layout>
       <a-layout-header>
         <div class="userInfo">
@@ -17,19 +17,72 @@
         <Nuxt />
       </a-layout-content>
     </a-layout>
+    <a-modal
+      :visible="isModalIsVisible"
+      width="500px"
+      :footer="null"
+      :destroy-on-close="true"
+      centered
+      @cancel="closeModal"
+    >
+      <h1>Change Password</h1>
+      <a-form>
+        <ValidationObserver ref="observer" tag="div">
+          <AppInput v-model="userObject.newUsername" label="User Name" />
+          <AppInput
+            v-model="userObject.oldPassword"
+            label="Enter Old Password"
+            placeholder="Enter Old Password"
+            required
+            rules="required"
+            input-type="password"
+          />
+          <AppInput
+            v-model="userObject.password"
+            label="Enter New Password"
+            placeholder="Enter New Password"
+            required
+            rules="required"
+            input-type="password"
+          />
+          <AppInput
+            v-model="userObject.confirmPassword"
+            label="Confirm Password"
+            placeholder="Confirm Password"
+            required
+            rules="required"
+            input-type="password"
+          />
+        </ValidationObserver>
+      </a-form>
+      <div>
+        <AppButton
+          class="custom-btn"
+          :loading="loading"
+          @click="changePassword()"
+          >Submit</AppButton
+        >
+      </div>
+    </a-modal>
   </a-layout>
 </template>
 <script>
 import AppAdminSideBar from '@/components/AppAdminSideBar.vue'
-
+import AppButton from '@/components/UI/AppButton.vue'
+import AppInput from '@/components/UI/AppInput.vue'
 export default {
   components: {
     AppAdminSideBar,
+    AppInput,
+    AppButton,
   },
   middleware: ['auth'],
   data() {
     return {
       userDigest: {},
+      isModalIsVisible: false,
+      loading: false,
+      userObject: {},
     }
   },
   computed: {
@@ -55,6 +108,59 @@ export default {
         newRole = newRole.includes('_') ? newRole.replace(/_/g, ' ') : newRole
       }
       return newRole
+    },
+    showChangePasswordModal() {
+      this.isModalIsVisible = true
+    },
+    closeModal() {
+      this.isModalIsVisible = false
+    },
+    async changePassword() {
+      const isValid = await this.$refs.observer.validate()
+      if (!isValid) {
+        return
+      }
+      const user = JSON.parse(localStorage.getItem('user'))
+      const config = {
+        headers: { Authorization: `Bearer ${user.token}` },
+      }
+      const payload = {
+        ...this.userObject,
+        oldUsername: this.userDigest.username,
+      }
+      this.loading = true
+      try {
+        const { response } = await this.$axios.$post(
+          '/user/updateCredentials',
+          payload,
+          config
+        )
+
+        if (response.loginAgain) {
+          this.goHome()
+        }
+        this.$notification.success({
+          message: 'Success',
+          description: 'Password Updated Successfully',
+          duration: 4000,
+        })
+        requestAnimationFrame(() => {
+          this.$refs.observer.reset()
+          this.userObject = {}
+          this.loading = false
+          this.closeModal()
+        })
+      } catch (err) {
+        this.loading = false
+        const { default: errorHandler } = await import('@/utils/errorHandler')
+        errorHandler(err).forEach((msg) => {
+          this.$notification.error({
+            message: 'Error',
+            description: msg,
+            duration: 0,
+          })
+        })
+      }
     },
   },
 }
